3e053cfac2d2be6869c378fc2f61553ffdfd51aa
Arfal Permana <m.arfalpermana@gmail.com>
2025-08-12 16:51:17 +0700

    Aug 12 2025

---
+++ basicloop/forloop.py
@@ -0,0 +1,12 @@
+# this is basic loop using for. what for?
+
+numbers = [1, 2, 3, 4, 5]
+
+for i in numbers:
+    print(f"i => {i}")
+
+for letter in "Hello World":  # they can loop words?
+    print(letter)
+
+for i in range(5):
+    print(f"i => {i}")
---
+++ basicloop/whileloop.py
@@ -0,0 +1,7 @@
+# this is also looping but using while.
+
+number = 1
+
+while number < 10:
+    print(number)
+    number += 1
